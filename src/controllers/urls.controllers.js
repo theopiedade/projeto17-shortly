9f27073b9ed1f3e2544369f086e8bc9750aa32da
Theo <theo.piedade@gmail.com>
2023-10-02 00:49:06 -0300

    POST, GET and DELETE

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -19,7 +19,6 @@ export async function urlShort(req, res) {
     const checkToken = await db.query(`SELECT * FROM signs WHERE token = $1;`, [token]);
     if (checkUser.rowCount <= 0) return res.sendStatus(401);
 
-    //if (!isValidUrl(url)) return res.sendStatus(422);
 
     const shortUrl = nanoid();
 
@@ -45,3 +44,62 @@ export async function urlShort(req, res) {
     }
 
 }
+
+export async function getUrl(req, res) {
+    const { id } = req.params;
+
+    const checkUrl = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id]);
+    if (checkUrl.rowCount <= 0) return res.sendStatus(404);
+
+    const data = {
+        id: id,
+        shortUrl: checkUrl.short,
+        url: checkUrl.url
+    }
+
+    return res.status(200).send(data);
+
+}
+
+export async function openShortUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    const checkUrl = await db.query(`SELECT * FROM urls WHERE short = $1;`, [shortUrl]);
+    if (checkUrl.rowCount <= 0) return res.sendStatus(404);
+
+    const count = parseInt(checkUrl.rows[0].views) + 1;
+
+    try {
+        await db.query(`UPDATE urls SET views = $1 WHERE short = $1;`, [shortUrl]);
+        res.redirect(checkUrl.rows[0].url);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+
+}
+
+export async function deleteUrl(req, res) {
+    const { id } = req.params;
+    const { authorization } = req.header;
+    const token = authorization?.replace('Bearer ', '');
+
+    const checkToken = await db.query(`SELECT * FROM signs WHERE token = $1;`, [token]);
+    if (checkUser.rowCount <= 0) return res.sendStatus(401);
+
+    const userId = checkToken.rows[0].userid;
+
+    const checkUrl = await db.query(`SELECT * FROM urls WHERE id = $1 AND userid = $2;`, [id, userId]);
+    if (checkUrl.rowCount <= 0) return res.sendStatus(404);
+
+    try{
+
+        await db.query(`DELETE from urls WHERE id = $1;`, [id]);
+        return res.status(204);
+
+    }catch(err){
+        return res.status(500).send(err.message);
+    }
+
+}
+
+
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -5,5 +5,7 @@ import { urlShort } from "../controllers/urls.controllers.js"
 const urlsRouter = Router()
 
 urlsRouter.post("/urls/shorten", validateSchema(schemaUrl), urlShort);
+urlsRouter.get("/urls/:id", getUrl);
+urlsRouter.get("/urls/open/:shortUrl", openShortUrl);
 
 export default urlsRouter
