de1712cc0017ed94d98dd35403e80c885e43f7e6
Theo <theo.piedade@gmail.com>
2023-10-02 00:53:25 -0300

    urlShort, getUrl, openShortUrl, deletUrl routes

--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { schemaUrl } from "../schemas/urls.schemas.js"
-import { urlShort } from "../controllers/urls.controllers.js"
+import { deleteUrl, urlShort, getUrl, openShortUrl } from "../controllers/urls.controllers.js"
 
 const urlsRouter = Router()
 
 urlsRouter.post("/urls/shorten", validateSchema(schemaUrl), urlShort);
 urlsRouter.get("/urls/:id", getUrl);
 urlsRouter.get("/urls/open/:shortUrl", openShortUrl);
+urlsRouter.delete("/urls/:id", deleteUrl);
 
 export default urlsRouter
